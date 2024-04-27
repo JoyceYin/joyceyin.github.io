2fe6ca5d177a9ee3937b94ddb212b350639d4724
JoyceYin <joyceyinxh@gmail.com>
2024-04-26 22:12:48 -0400

    update ev data

---
+++ statics/js/ev_adoption.js
@@ -0,0 +1,6 @@
+
+function EVadoption_line(id, data){
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+}
