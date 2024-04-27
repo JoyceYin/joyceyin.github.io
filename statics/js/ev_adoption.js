90dc2c0838a68642412f30aa97d5b8b655e1abb5
JoyceYin <joyceyinxh@gmail.com>
2024-04-26 22:34:05 -0400

    Update ev_adoption.js

--- statics/js/ev_adoption.js
+++ statics/js/ev_adoption.js
@@ -3,4 +3,65 @@ function EVadoption_line(id, data){
     var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select(id)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    //Read the data
+    d3.csv(data, function(data) {
+
+        console.log(data)
+    // group the data: I want to draw one line per group
+    var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+    .key(function(d) { return d.Borough;})
+    .entries(data);
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) { return d.Year; }))
+    .range([ 0, width ]);
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).ticks(13));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return +d['Vehicle Count']; })])
+    .range([ height, 0 ]);
+    svg.append("g")
+    .call(d3.axisLeft(y));
+
+    // color palette
+    var res = sumstat.map(function(d){ return d.key }) // list of group names
+    var color = d3.scaleOrdinal()
+    .domain(res)
+    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00'])
+
+    // Draw the line
+    svg.selectAll(".line")
+        .data(sumstat)
+        .enter()
+        .append("path")
+            .attr("fill", "none")
+            .attr("stroke", function(d){ return color(d.key) })
+            .attr("stroke-width", 3)
+            .attr("d", function(d){
+            return d3.line()
+                .x(function(d) { return x(d.Year); })
+                .y(function(d) { return y(+d['Vehicle Count']); })
+                (d.values)
+            })
+
+    })
+
 }
+
+var ev_adopt_boro = 'https://raw.githubusercontent.com/JoyceYin/joyceyin.github.io/main/statics/data/ev_adoption_boro_year.csv'
+var ev_adopt_id = '#ev_adoption_boro'
+EVadoption_line(ev_adopt_id, ev_adopt_boro)
